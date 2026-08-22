5367cddc244cac9e381fdadeb9817f0bbe25382e
coder-ali-ba <habibali8770@gmail.com>
2026-08-23 04:21:32 +0500

    hssss

--- src/services/projectApis.js
+++ src/services/projectApis.js
@@ -1,82 +1,113 @@
+// import axios from "axios";
+// import Cookies from "js-cookie";
+
+// const API_URL =
+//   "https://hackathon-backend-seven-jet.vercel.app/api/project";
+
+// const getHeaders = () => {
+//   const token = Cookies.get("token");
+
+//   return {
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//   };
+// };
+
+// // GET ALL PROJECTS
+// export const getProjects = async () => {
+//   const response = await axios.get(
+//     `${API_URL}/getallProjects`,
+//     getHeaders()
+//   );
+
+//   return response.data;
+// };
+
+// // CREATE PROJECT
+// export const createProject = async (data) => {
+//   const response = await axios.post(
+//     `${API_URL}/create`,
+//     data,
+//     getHeaders()
+//   );
+
+//   return response.data;
+// };
+
+// // UPDATE PROJECT
+// export const updateProject = async (id, data) => {
+//   const response = await axios.put(
+//     `${API_URL}/${id}`,
+//     data,
+//     getHeaders()
+//   );
+
+//   return response.data;
+// };
+
+// // DELETE PROJECT
+// export const deleteProject = async (id) => {
+//   const response = await axios.delete(
+//     `${API_URL}/${id}`,
+//     getHeaders()
+//   );
+
+//   return response.data;
+// };
+
+// // APPROVE PROJECT
+// export const approveProject = async (id) => {
+//   const response = await axios.put(
+//     `${API_URL}/${id}`,
+//     {
+//       status: "approved",
+//     },
+//     getHeaders()
+//   );
+
+//   return response.data;
+// };
+
+// // REJECT PROJECT
+// export const rejectProject = async (id) => {
+//   const response = await axios.put(
+//     `${API_URL}/${id}`,
+//     {
+//       status: "rejected",
+//     },
+//     getHeaders()
+//   );
+
+//   return response.data;
+// };
+
 import axios from "axios";
 import Cookies from "js-cookie";
 
 const API_URL =
   "https://hackathon-backend-seven-jet.vercel.app/api/project";
 
-const getHeaders = () => {
+const getConfig = () => {
   const token = Cookies.get("token");
+
+  console.log("TOKEN FROM COOKIE:", token);
 
   return {
     headers: {
       Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
     },
   };
 };
 
-// GET ALL PROJECTS
-export const getProjects = async () => {
-  const response = await axios.get(
-    `${API_URL}/getallProjects`,
-    getHeaders()
-  );
-
-  return response.data;
-};
-
-// CREATE PROJECT
 export const createProject = async (data) => {
+  console.log("PROJECT DATA:", data);
+
   const response = await axios.post(
     `${API_URL}/create`,
     data,
-    getHeaders()
-  );
-
-  return response.data;
-};
-
-// UPDATE PROJECT
-export const updateProject = async (id, data) => {
-  const response = await axios.put(
-    `${API_URL}/${id}`,
-    data,
-    getHeaders()
-  );
-
-  return response.data;
-};
-
-// DELETE PROJECT
-export const deleteProject = async (id) => {
-  const response = await axios.delete(
-    `${API_URL}/${id}`,
-    getHeaders()
-  );
-
-  return response.data;
-};
-
-// APPROVE PROJECT
-export const approveProject = async (id) => {
-  const response = await axios.put(
-    `${API_URL}/${id}`,
-    {
-      status: "approved",
-    },
-    getHeaders()
-  );
-
-  return response.data;
-};
-
-// REJECT PROJECT
-export const rejectProject = async (id) => {
-  const response = await axios.put(
-    `${API_URL}/${id}`,
-    {
-      status: "rejected",
-    },
-    getHeaders()
+    getConfig()
   );
 
   return response.data;
