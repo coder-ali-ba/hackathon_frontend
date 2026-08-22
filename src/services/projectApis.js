be47fcdfce92707f76178f78c2079977b3167fac
coder-ali-ba <habibali8770@gmail.com>
2026-08-23 04:16:35 +0500

    fronttttt

--- src/services/projectApis.js
+++ src/services/projectApis.js
@@ -1,57 +1,82 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const API_URL =
   "https://hackathon-backend-seven-jet.vercel.app/api/project";
 
+const getHeaders = () => {
+  const token = Cookies.get("token");
+
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
+// GET ALL PROJECTS
 export const getProjects = async () => {
-  const response = await axios.get(`${API_URL}/getallProjects`);
+  const response = await axios.get(
+    `${API_URL}/getallProjects`,
+    getHeaders()
+  );
+
   return response.data;
 };
 
-export const getProjectById = async (id) => {
-  const response = await axios.get(`${API_URL}/${id}`);
-  return response.data;
-};
-
+// CREATE PROJECT
 export const createProject = async (data) => {
-  const response = await axios.post(`${API_URL}/create`, data);
+  const response = await axios.post(
+    `${API_URL}/create`,
+    data,
+    getHeaders()
+  );
+
   return response.data;
 };
 
+// UPDATE PROJECT
 export const updateProject = async (id, data) => {
   const response = await axios.put(
     `${API_URL}/${id}`,
-    data
+    data,
+    getHeaders()
   );
 
   return response.data;
 };
 
+// DELETE PROJECT
 export const deleteProject = async (id) => {
   const response = await axios.delete(
-    `${API_URL}/${id}`
+    `${API_URL}/${id}`,
+    getHeaders()
   );
 
   return response.data;
 };
 
+// APPROVE PROJECT
 export const approveProject = async (id) => {
   const response = await axios.put(
     `${API_URL}/${id}`,
     {
-      status: "Approved",
-    }
+      status: "approved",
+    },
+    getHeaders()
   );
 
   return response.data;
 };
 
+// REJECT PROJECT
 export const rejectProject = async (id) => {
   const response = await axios.put(
     `${API_URL}/${id}`,
     {
-      status: "Rejected",
-    }
+      status: "rejected",
+    },
+    getHeaders()
   );
 
   return response.data;
