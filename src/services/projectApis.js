1f29835d606e64e45163321557ec7f6119d6be63
coder-ali-ba <habibali8770@gmail.com>
2026-08-23 04:33:23 +0500

    heeeeelll

--- src/services/projectApis.js
+++ src/services/projectApis.js
@@ -1,87 +1,3 @@
-// import axios from "axios";
-// import Cookies from "js-cookie";
-
-// const API_URL =
-//   "https://hackathon-backend-seven-jet.vercel.app/api/project";
-
-// const getHeaders = () => {
-//   const token = Cookies.get("token");
-
-//   return {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   };
-// };
-
-// // GET ALL PROJECTS
-// export const getProjects = async () => {
-//   const response = await axios.get(
-//     `${API_URL}/getallProjects`,
-//     getHeaders()
-//   );
-
-//   return response.data;
-// };
-
-// // CREATE PROJECT
-// export const createProject = async (data) => {
-//   const response = await axios.post(
-//     `${API_URL}/create`,
-//     data,
-//     getHeaders()
-//   );
-
-//   return response.data;
-// };
-
-// // UPDATE PROJECT
-// export const updateProject = async (id, data) => {
-//   const response = await axios.put(
-//     `${API_URL}/${id}`,
-//     data,
-//     getHeaders()
-//   );
-
-//   return response.data;
-// };
-
-// // DELETE PROJECT
-// export const deleteProject = async (id) => {
-//   const response = await axios.delete(
-//     `${API_URL}/${id}`,
-//     getHeaders()
-//   );
-
-//   return response.data;
-// };
-
-// // APPROVE PROJECT
-// export const approveProject = async (id) => {
-//   const response = await axios.put(
-//     `${API_URL}/${id}`,
-//     {
-//       status: "approved",
-//     },
-//     getHeaders()
-//   );
-
-//   return response.data;
-// };
-
-// // REJECT PROJECT
-// export const rejectProject = async (id) => {
-//   const response = await axios.put(
-//     `${API_URL}/${id}`,
-//     {
-//       status: "rejected",
-//     },
-//     getHeaders()
-//   );
-
-//   return response.data;
-// };
-
 import axios from "axios";
 import Cookies from "js-cookie";
 
@@ -91,22 +7,75 @@ const API_URL =
 const getConfig = () => {
   const token = Cookies.get("token");
 
-  console.log("TOKEN FROM COOKIE:", token);
+  console.log("TOKEN:", token);
 
   return {
     headers: {
       Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
     },
   };
 };
 
-export const createProject = async (data) => {
-  console.log("PROJECT DATA:", data);
+// GET ALL PROJECTS
+export const getProjects = async () => {
+  const response = await axios.get(
+    `${API_URL}/getallProjects`,
+    getConfig()
+  );
 
+  return response.data;
+};
+
+// CREATE PROJECT
+export const createProject = async (data) => {
   const response = await axios.post(
     `${API_URL}/create`,
     data,
+    getConfig()
+  );
+
+  return response.data;
+};
+
+// UPDATE PROJECT
+export const updateProject = async (id, data) => {
+  const response = await axios.put(
+    `${API_URL}/${id}`,
+    data,
+    getConfig()
+  );
+
+  return response.data;
+};
+
+// DELETE PROJECT
+export const deleteProject = async (id) => {
+  const response = await axios.delete(
+    `${API_URL}/${id}`,
+    getConfig()
+  );
+
+  return response.data;
+};
+
+export const approveProject = async (id) => {
+  const response = await axios.put(
+    `${API_URL}/${id}`,
+    {
+      status: "approved",
+    },
+    getConfig()
+  );
+
+  return response.data;
+};
+
+export const rejectProject = async (id) => {
+  const response = await axios.put(
+    `${API_URL}/${id}`,
+    {
+      status: "rejected",
+    },
     getConfig()
   );
 
